1f466f0b49fccbc2b3e3533835cc7967708678ed
ducaiwei <DUCAIWEI302@pingan.com.cn>
2016-12-21 18:03:32 +0800

    'scrolllist'

--- src/view/ScrollerList.vue
+++ src/view/ScrollerList.vue
@@ -1,13 +1,34 @@
 <style>
-
+.list-box{
+  display: block;
+  height: 300px;
+}
 </style>
 <template>
-
+<div>
+  <scroller height="100%" lockX scrollbarY="true">
+    <ul class="list-box">
+      <li v-for="data in list">
+        {{data}}
+      </li>
+    </ul>
+  </scroller>
+</div>
 </template>
 <script>
   import Scroller from '../components/Scroller.vue'
   export default {
     name: 'scroller-list',
+    data() {
+      return {
+        list:[]
+      }
+    },
+    created() {
+      for(var i = 0 ; i < 50 ;i++) {
+        this.list.push(i);
+      }
+    },
     components: {
       scroller: Scroller
     }
