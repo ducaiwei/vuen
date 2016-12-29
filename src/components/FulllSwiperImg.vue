9e9a71f0972ff00ef602fd454d97fa7fbebb1214
ducaiwei <DUCAIWEI302@pingan.com.cn>
2016-12-29 18:20:28 +0800

    'FullImg'

--- src/components/FulllSwiperImg.vue
+++ src/components/FulllSwiperImg.vue
@@ -2,8 +2,20 @@
 
 </style>
 <template>
+  <div>
 
+  </div>
 </template>
 <script>
-
+  import Swipe from 'Swipe'
+  import SwipeItem from 'SwipeItem'
+  import Scroller from 'Scroller'
+  export default {
+    props: {
+      imgs: {
+        Type:Array,
+        default:[]
+      }
+    }
+  }
 </script>
